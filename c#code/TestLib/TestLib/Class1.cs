91fdbf293ab3d2957a0004a8a45f3a68353636ae
ksprul <karl@sprules.com>
2019-03-05 19:09:57 -0600

    c#code, etc

---
+++ c#code/TestLib/TestLib/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
+
+namespace TestLib
+{
+    public class Test
+    {
+
+        /// <summary>
+        /// SIMPL+ can only execute the default constructor. If you have variables that require initialization, please
+        /// use an Initialize method
+        /// </summary>
+        public Test()
+        {
+        }
+        public int ReturnInt()
+        {
+            return (10);
+        }
+    }
+}
